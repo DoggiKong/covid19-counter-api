98f56299ca68de7594ea0308bf160d0f9dddadd8
Jerry Wang <jerryw4113@gmail.com>
2020-03-19 20:05:59 +1000

    Added CORS

--- src/main/kotlin/com/pandabyte/covid19counterapi/v1/controllers/CovidStatisticController.kt
+++ src/main/kotlin/com/pandabyte/covid19counterapi/v1/controllers/CovidStatisticController.kt
@@ -4,9 +4,11 @@ import com.pandabyte.covid19counterapi.CovidStatisticService
 import com.pandabyte.covid19counterapi.v1.models.CovidStatisticCountries
 import com.pandabyte.covid19counterapi.v1.models.CovidStatisticSummary
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["https://covid19-counter.herokuapp.com/"])
 @RestController
 class CovidStatisticController(
         @Autowired
