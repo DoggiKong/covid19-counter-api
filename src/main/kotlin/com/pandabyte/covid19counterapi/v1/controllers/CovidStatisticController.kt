8ac466af1a363ac3069704e4bfadb003c138839f
Jerry Wang <jerryw4113@gmail.com>
2020-03-19 20:36:59 +1000

    Added CORS for heroku

--- src/main/kotlin/com/pandabyte/covid19counterapi/v1/controllers/CovidStatisticController.kt
+++ src/main/kotlin/com/pandabyte/covid19counterapi/v1/controllers/CovidStatisticController.kt
@@ -6,20 +6,22 @@ import com.pandabyte.covid19counterapi.v1.models.CovidStatisticSummary
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin(origins = ["https://covid19-counter.herokuapp.com/"])
+@CrossOrigin(origins = ["https://covid19-counter.herokuapp.com"], maxAge = 3600)
 @RestController
+@RequestMapping("/v1/statistics")
 class CovidStatisticController(
         @Autowired
         val covidStatisticService: CovidStatisticService
 ) {
-    @GetMapping("/v1/statistics/summary")
+    @GetMapping("/summary")
     fun getCovidStatistics(): CovidStatisticSummary {
         return covidStatisticService.getCovidStatisticSummary()
     }
 
-    @GetMapping("/v1/statistics/countries")
+    @GetMapping("/countries")
     fun getCovidStatisticsCountries(): CovidStatisticCountries {
         return covidStatisticService.getCovidCountriesList()
     }
